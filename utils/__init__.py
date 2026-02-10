bc0e38627dacbb038875d9ed8edfaf68745eb605
emma otero <64655124+emmaotero@users.noreply.github.com>
2026-02-10 14:26:00 -0300

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,48 @@
+"""
+Paquete de utilidades para Portfolio Manager
+"""
+
+from .auth import init_supabase, login_user, register_user, logout_user
+from .styles import apply_custom_styles
+from .market_data import (
+    get_current_price, 
+    get_stock_info, 
+    get_historical_data,
+    get_technical_indicators,
+    interpret_rsi,
+    interpret_macd,
+    interpret_sma,
+    interpret_bollinger
+)
+from .database import (
+    get_user_positions,
+    add_position,
+    update_position,
+    delete_position,
+    get_investor_profile,
+    update_investor_profile,
+    calculate_portfolio_metrics
+)
+
+__all__ = [
+    'init_supabase',
+    'login_user',
+    'register_user',
+    'logout_user',
+    'apply_custom_styles',
+    'get_current_price',
+    'get_stock_info',
+    'get_historical_data',
+    'get_technical_indicators',
+    'interpret_rsi',
+    'interpret_macd',
+    'interpret_sma',
+    'interpret_bollinger',
+    'get_user_positions',
+    'add_position',
+    'update_position',
+    'delete_position',
+    'get_investor_profile',
+    'update_investor_profile',
+    'calculate_portfolio_metrics'
+]
